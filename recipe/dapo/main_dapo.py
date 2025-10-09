03320a3ad20df1ee9732b15cbb600c3e515d5e11
Serge-weihao <40890323+Serge-weihao@users.noreply.github.com>
2025-10-10 00:36:51 +0800

    Update main_dapo.py

--- recipe/dapo/main_dapo.py
+++ recipe/dapo/main_dapo.py
@@ -87,8 +87,8 @@ class TaskRunner:
         # instantiate tokenizer
         from verl.utils import hf_processor, hf_tokenizer
 
-        tokenizer = hf_tokenizer(local_path)
-        processor = hf_processor(local_path, use_fast=True)  # used for multimodal LLM, could be none
+        tokenizer = hf_tokenizer(local_path, trust_remote_code=True)
+        processor = hf_processor(local_path, use_fast=True, trust_remote_code=True)  # used for multimodal LLM, could be none
 
         # define worker classes
         if config.actor_rollout_ref.actor.strategy == "fsdp":
